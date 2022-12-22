d225431178db9a436b100eaf027cac2082add30d
Doğan Seyfi Şen <118891768+doganseyfisen@users.noreply.github.com>
2022-12-22 22:30:46 +0300

    Create ifadeler.java

---
+++ ifadeler.java
@@ -0,0 +1,20 @@
+package iFadeler;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		//int i; 
+		//expression
+		
+		//int x = 23; 
+		//declaration
+		System.out.println("Hi");
+		double pow = Math.pow(12, 3);
+		System.out.println(pow);
+		
+		for (int i = 0; i <= 10; i++) {
+		System.out.println(i);
+		}
+	}
+
+}
