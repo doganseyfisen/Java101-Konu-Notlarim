cb73c9112aae78e67526f72bdf8148d7606c73d3
Doğan Seyfi Şen <118891768+doganseyfisen@users.noreply.github.com>
2022-12-22 22:31:08 +0300

    Create if-block.java

---
+++ if-block.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		/*
+		int number = 10;
+		
+		boolean result = number > 0;
+		
+		if (result) {
+			System.out.println("Number is bigger than zero.");
+		} else {
+			System.out.println("Number is less than zero.");
+		}
+		
+		System.out.println("Burası daima çalıştırılacak.");
+		*/
+		
+		//kullanıcıdan değer al
+		
+		/*
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please write a number: ");
+		int number = scanner.nextInt();
+		if (number > 0) {
+			System.out.println("Number is bigger than zero.");
+		} else if (number < 0) {
+			System.out.println("Number is less than zero.");
+		} else {
+			System.out.println("Number is equal to zero.");
+		}
+		System.out.println(number%3);
+		*/
+		
+		/*
+		int number = 75;
+		if (number < 100) {
+			System.out.println("Sayı 100'den küçüktür.");
+			if (number > 50) {
+				System.out.println("Sayı 50'den büyüktür.");
+			}
+		*/
+		
+		/*
+		Scanner scanner = new Scanner(System.in);
+			System.out.print("Lütfen bir sayı giriniz: ");
+			int number = scanner.nextInt();
+			if(number < 100 && number >=10) {
+				System.out.println("Girdiğiniz sayı 2 basamaklıdır.");
+			} else if (number > 99 && number <= 999) {
+				System.out.println("Girdiğiniz sayı 3 basamaklıdır.");
+			}
+		*/
+		
+		
+		}
+	}
+
+
