b514a71ca82eb9cf4f6d07af7abb5d65fec78ba8
Doğan Seyfi Şen <118891768+doganseyfisen@users.noreply.github.com>
2022-12-22 22:27:45 +0300

    Create for-loop.java

---
+++ for-loop.java
@@ -0,0 +1,59 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		/*
+		for(int i = 5; i >= -5; i--) {
+			System.out.println(i);
+		}
+		*/
+		
+		/*
+		int[] numbers = {10, 5, -4, 6, 3, 9};
+			for(int i = 0; i < numbers.length; i++) {
+				System.out.println(i);
+			}
+		*/
+		
+		/*
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter number: ");
+		int x = scanner.nextInt();
+			for(int i = 0; i <= x; i++) {
+				System.out.println(i);	
+			}
+		*/	
+			
+		/*
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter number: ");
+		int x = scanner.nextInt();
+			for(int i = 0; i <= x; i++) {
+				if(i % 2 == 0) {
+				System.out.println(i);
+				}	
+			}
+		*/
+		
+		/*
+		String[] names;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Kaç kişi girilecek: ");
+		names = new String[Integer.parseInt(scanner.nextLine())]; 
+		//başta int değeri girdik ve names adı altında int'i string değerine denkledik
+		
+		for (int i = 0; i < names.length; i++) {
+			System.out.print((i+1)+ ". kişi: ");
+			names[i] = scanner.nextLine();
+		}
+		
+		for (int i = 0; i < names.length; i++) {
+			System.out.print(names[i] + ", ");
+		}
+		*/
+		
+	}
+}
