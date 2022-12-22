26f3f73b73435b2805aa1a6ff6d98c6e57536262
Doğan Seyfi Şen <118891768+doganseyfisen@users.noreply.github.com>
2022-12-22 22:46:45 +0300

    Create string.java

---
+++ string.java
@@ -0,0 +1,17 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		String firstName = "Jake";
+		System.out.println(firstName.charAt(2));
+		System.out.println(firstName.toUpperCase());
+		System.out.println(firstName.toLowerCase());
+		System.out.println(firstName.replace('k', 'n'));
+		System.out.println(firstName.length());
+		System.out.println(firstName.substring(2)); 
+		//2. indeks ve sonrasını alır
+		System.out.println(firstName);
+
+	}
+
+}
