004db67f9b76970182c1f97469c34ec38e8b3581
Doğan Seyfi Şen <118891768+doganseyfisen@users.noreply.github.com>
2022-12-22 22:46:25 +0300

    Create static-methods.java

---
+++ static-methods.java
@@ -0,0 +1,21 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		
+		Math.pow(2, 3); //2 nin 3. kuvveti
+		System.out.println(Math.pow(2, 3));
+		System.out.println(Math.sqrt(100));
+		System.out.println(Math.min(8, 4));
+		System.out.println(Math.max(100, 101));
+		
+		float f = 1.51f;
+		System.out.println(Math.ceil(f)); //yukarı yuvarlar
+		System.out.println(Math.floor(f)); //aşağı yuvarlar
+		System.out.println(Math.round(f)); //hangi tam sayıya yakınsa yuvarlar
+		System.out.println(Math.random()); //random 0-1 arası sayı, 0 dahil 1 hariç
+		
+	}
+
+}
