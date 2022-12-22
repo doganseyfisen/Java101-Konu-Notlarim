b17ea253234869a4a8da4c8078f9706c2689b433
Doğan Seyfi Şen <118891768+doganseyfisen@users.noreply.github.com>
2022-12-22 22:42:48 +0300

    Create mantiksal-operatorler-lab.java

---
+++ mantiksal-operatorler-lab.java
@@ -0,0 +1,16 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		boolean a = false;
+		boolean b = true;
+		
+		System.out.println(a && b); 
+		//2. koşula true ise geçer
+		
+		System.out.println(a || b);
+		System.out.println(!a); 
+		// a nın değili
+	}
+
+}
