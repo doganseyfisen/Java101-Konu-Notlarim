024829212e59a716bb9be0ac758717340c43ba0a
Doğan Seyfi Şen <118891768+doganseyfisen@users.noreply.github.com>
2022-12-22 22:50:29 +0300

    Create while-loop.java

---
+++ while-loop.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+import java.util.*;
+
+public class Driver {
+	public static void main(String[] args) throws InterruptedException {
+		
+		/*
+		0'dan 9'a kadar i saydırma
+		int i = 0;
+		while (i < 10) {
+		System.out.println(i);
+		i++;
+		*/
+		
+		/*
+		saat uygulaması
+		while(true) {
+			System.out.println(new Date());
+			Thread.sleep(1000);
+		*/	
+		
+		/*
+		Scanner scanner = new Scanner(System.in);
+			//System.out.print("Enter a word or words: ");
+				System.out.print("Enter number: ");
+		while (true) {
+			//String text = scanner.nextLine();
+			//System.out.println(text.toUpperCase());
+				int i = scanner.nextInt();
+				System.out.println("Result: "+Math.pow(i, 4));
+		*/
+		}
+	}
